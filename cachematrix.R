d06f92138cfc68a5e0763e349da119b54f68045b
PrajaktaC <prajakta.chakurkar@gmail.com>
2015-05-22 21:51:03 -0700

    Update cachematrix.R
    added some comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    # assign the insvertedMatrix a value of NULL
     invertedMatrix <- NULL
+    
+    # define a function that sets the values of x and invertedMatrix in a different scope
     set <- function(y) {
         x <<- y
         invertedMatrix <<- NULL
     }
+    
+    # function that returns the value of x
     get <- function() x
+    
+    # function to set te inverted value of the matrix
     setInverseMatrix <- function(inverse) invertedMatrix <<- inverse
+    
+    # function to get the inverted matrix.
     getInverseMatrix <- function() invertedMatrix
     
     list (set=set, get=get,
@@ -23,12 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    
+        # assign a variable the inverted value of matrix x.
         invertedMatrix <- x$getInverseMatrix()
+        
+        #if the value is not null, that means it was cached.
         if (!is.null(invertedMatrix)) {
+            # print a message that the cached value is being returned.
             message ("getting cached data")
             return(invertedMatrix)
         }
+        # at this point, the value was not found in the cache. So, compute the inverse and return the value.
         data <- x$get()
         invertedMatrix <- solve(data)
         x$setInverseMatrix(invertedMatrix)
